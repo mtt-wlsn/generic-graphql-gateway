940f37f2c024089d557a8857b9334dab7a6f945a
mtt-wlsn <74565505+mtt-wlsn@users.noreply.github.com>
2023-03-10 08:39:31 -0600

    Added Gateway import.

--- src/gateway.module.ts
+++ src/gateway.module.ts
@@ -1,9 +1,34 @@
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GatewayController } from './gateway.controller';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      server: {
+        cors: true,
+        playground: false,
+        plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      },
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          pollIntervalInMs: 5000,
+          subgraphs: [
+            {
+              name: 'inventory',
+              url: `http://localhost:${process.env.INVENTORY_PORT}/graphql`,
+            },
+          ],
+        }),
+      },
+    }),
+  ],
   controllers: [GatewayController],
 })
 export class GatewayModule {}
